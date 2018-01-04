6251af1d0f8556d2e0629b183f0ff0e3050d9bd9
Jon Edwards <jedwards.langara@gmail.com>
2018-01-04 08:34:18 -0800

    some minor visual updates

--- js/script.js
+++ js/script.js
@@ -2,11 +2,9 @@
 var myTimer = setInterval(setClock, 1000);
 
 function setClock() {
-    document.querySelector(".time").innerHTML = moment().format('h:mm');
-    document.querySelector(".meridiem").innerHTML = moment().format('a');
+    document.querySelector(".time").innerHTML = moment().format('h:mm a');
     document.querySelector(".date").innerHTML = moment().format('LL');
 };
-
 
 $(document).ready(function() {
   $.simpleWeather({
@@ -14,7 +12,7 @@ $(document).ready(function() {
     woeid: '',
     unit: 'c',
     success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';  
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
 
       $(".weather").html(html);
     },
