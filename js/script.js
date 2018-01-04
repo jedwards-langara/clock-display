b66de29c7134b4a9bfa274a4c7615cf099d3c2eb
Jon Edwards <jedwards.langara@gmail.com>
2018-01-04 15:28:04 -0800

    Added js for dynamic greeting and background image, as well as changing meridiem based on time of day

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Control TIME
+//Control TIME w/ Moment.js
 var myTimer = setInterval(setClock, 1000);
 
 function setClock() {
@@ -7,6 +7,7 @@ function setClock() {
 
 };
 
+//Simple Weather JQUERY Module to display current local temperature
 $(document).ready(function() {
   $.simpleWeather({
     location: 'Vancouver, BC',
@@ -15,24 +16,34 @@ $(document).ready(function() {
     success: function(weather) {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;</h2>';
 
-      $(".weather").html(html);
+      $('.weather').html(html);
     },
     error: function(error) {
-      $(".weather").html('<p>'+error+'</p>');
+      $('.weather').html('<p>'+error+'</p>');
     }
   });
 });
 
+//Change Meridiem, greeting and background image based on time of day.
 function timeOfDay( ) {
 
-    var clockMeridiem = document.querySelector(".meridiem");
+    var clockMeridiem = document.querySelector('.meridiem');
     var hour = new Date().getHours();
+    var greetingMsg = document.querySelector('.greeting')
+    var backgroundIMG = document.getElementById('container')
 
-        if ( hour < 12 )  {
+    if (hour < 12)  {
         clockMeridiem.innerHTML = 'am';
-        }
-        else {
+        greetingMsg.innerHTML = 'Good morning';
+        backgroundIMG.style.background = "url('/img/clouds.jpg')"
+    } if (hour < 18 && hour > 12 ) {
         clockMeridiem.innerHTML = 'pm';
-        console.log(hour)
-        }
+        greetingMsg.innerHTML = 'Good afternoon';
+        backgroundIMG.style.background = "url('/img/clear.jpg')"
+    } else {
+        clockMeridiem.innerHTML = 'pm';
+        greetingMsg.innerHTML = 'Goodnight';
+        backgroundIMG.style.background = "url('/img/night.jpg')"
+
+    }
 }
